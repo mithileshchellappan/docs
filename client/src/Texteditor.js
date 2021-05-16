f522d12525a6303816e25003fe1b3e2b79268a12
mithileshchellappan <35962266+mithileshchellappan@users.noreply.github.com>
2021-05-16 17:53:13 +0530

    connect client and server

--- client/src/Texteditor.js
+++ client/src/Texteditor.js
@@ -1,21 +1,51 @@
-import React,{useCallback} from 'react'
-import Quill from 'quill'
-import 'quill/dist/quill.snow.css'
+import React, { useCallback, useEffect, useState } from "react";
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
+import { io, Socket } from "socket.io-client";
+const TOOLBAR_OPTIONS = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  ["bold", "italic", "underline"],
+  [{ color: [] }, { background: [] }],
+  [{ script: "sub" }, { script: "super" }],
+  [{ align: [] }],
+  ["image", "blockquote", "code-block"],
+  ["clean"],
+];
 
 export default function Texteditor() {
-    // const wrapperRef = useRef()
-    const wrapperRef=useCallback((wrapper) => {
-        if(wrapper==null)return
-        wrapper.innerHTML=''
-        const editor = document.createElement('div')
-        wrapper.append(editor)
-        new Quill(editor,{theme:'snow'})
+  const [socket, setSocket] = useState();
+  const [quill, setQuill] = useState();
 
-        
-    }, [])
-    return (
-        <div className="container" ref={wrapperRef}>
-            
-        </div>
-    )
+  useEffect(() => {
+    const s = io("http://localhost:3001");
+    setSocket(s);
+    return () => {
+      s.disconnect();
+    };
+  }, []);
+  const wrapperRef = useCallback((wrapper) => {
+    if (wrapper == null) return;
+    wrapper.innerHTML = "";
+    const editor = document.createElement("div");
+    wrapper.append(editor);
+    const q = new Quill(editor, {
+      theme: "snow",
+      modules: { toolbar: TOOLBAR_OPTIONS },
+    });
+    setQuill(q);
+  }, []);
+  useEffect(() => {
+      if(socket==null||quill==null)return
+      const handler=(delta,oldDelta,source)=>{
+        if(source !=='user') return
+        socket.emit('send-changes',delta)
+    }
+      quill.on('text-change',handler)
+      return()=>{
+          quill.of('text-change',handler)
+      }
+  },[socket,quill]);
+  return <div className="container" ref={wrapperRef}></div>;
 }
